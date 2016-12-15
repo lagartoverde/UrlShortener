1965a594e1562f271f8a5f6ad728d4cd1edea9cf
Oscar Rodriguez Villalobos <lagartoverde97@gmail.com>
2016-12-15 14:52:02 +0100

    Added the routing

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ var http=require("http");
 var url=require("url");
 
 var server=http.createServer(function(req,res){
-	
+	var requestName=url.parse(req.url).pathname;
+	res.end(requestName);
 });
 server.listen(process.env.PORT ||8080);
 console.log("Server Running");
